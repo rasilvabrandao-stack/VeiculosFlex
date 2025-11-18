0fec10dd5099d0c5395e88e3e5342a02bfd172b8
rasilvabrandao-stack <rasilvabrandao-stack@users.noreply.github.com>
2025-11-18 14:53:30 -0300

    Remove --only-binary=all from requirements.txt to allow building packages from source if needed

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ apt-get update && apt-get install -y \
     zlib1g-dev \
     && rm -rf /var/lib/apt/lists/*
 
+# Upgrade pip
+python -m pip install --upgrade pip
+
 # Install Python dependencies
 pip install -r requirements.txt
